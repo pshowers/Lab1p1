a05f3034a047757cb738daaacc1307017681b63f
Phillip Showers <phillipshowers@Phillips-MacBook-Air.local>
2015-02-13 16:37:27 -0700

    Added delayUs(); to timer.c

--- timer.c
+++ timer.c
@@ -8,8 +8,22 @@
 #include "p24fj64ga002.h"
 #include "timer.h"
 
-//Uses timer 2
-void delayUs(unsigned int delay)
-{
-    //TODO: Create a delay using timer 2 for "delay" microseconds.
+#define FCY 14745600
+#define ONE_MILLISECOND 57
+
+/* Delay 1 millisecond * the input (delay)
+ */
+void delayUs(unsigned int delay){
+    TMR2 = 0; //According to the datasheet this should be reset automatically, but it does not sometimes.
+    PR2 = delay*ONE_MILLISECOND; //Period register value calculated from lecture
+    T2CONbits.TCKPS = 0b11; //prescalar 256
+    IFS0bits.T2IF = 0; // Timer 2 interrupt flag down
+    T2CONbits.TON = 1; // Turn timer 2 on
+
+
+    // Wait until the timer 2 interrupt flag goes up. This is done in hardware.
+    while(IFS0bits.T2IF == 0);
+
+    IFS0bits.T2IF = 0; // Put the flag down afterwards.
+    T2CONbits.TON = 0; // Turn the timer off so it does not keep counting.
 }
